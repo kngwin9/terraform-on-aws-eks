d457b1d71465ce7f99eee50cb2797b1bd279f68e
Kevin Nguyen <kngwin9@gmail.com>
2025-08-19 16:55:00 -0700

    Saving

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
@@ -2,20 +2,19 @@
 data "aws_ami" "amzlinux2" {
   most_recent      = true
   owners           = ["amazon"]
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
+    values = ["myami-*"]
   }
+
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
   }
 }
