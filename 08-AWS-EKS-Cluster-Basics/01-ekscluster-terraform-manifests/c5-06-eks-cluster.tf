980f7d1c5560cbbedaa598719c6b40f1a23ba3d2
Kevin Nguyen <kngwin9@gmail.com>
2025-08-24 15:26:38 -0700

    updated eks cluster version to latest

--- 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c5-06-eks-cluster.tf
+++ 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/c5-06-eks-cluster.tf
@@ -1,27 +1,50 @@
 # Create AWS EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "${local.name}-${var.cluster_name}"
+  name = "${local.name}-${var.cluster_name}"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
   role_arn = aws_iam_role.eks_master_role.arn
   version = var.cluster_version
 
   vpc_config {
-    subnet_ids = module.vpc.public_subnets
-    endpoint_private_access = var.cluster_endpoint_private_access
-    endpoint_public_access  = var.cluster_endpoint_public_access
-    public_access_cidrs     = var.cluster_endpoint_public_access_cidrs    
+    subnet_ids = [
+      aws_subnet.az1.id,
+      aws_subnet.az2.id,
+      aws_subnet.az3.id,
+    ]
   }
 
-  kubernetes_network_config {
-    service_ipv4_cidr = var.cluster_service_ipv4_cidr
-  }
-  
-  # Enable EKS Cluster Control Plane Logging
-  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
   ]
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
 }
